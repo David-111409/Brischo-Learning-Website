c220b5a3ce135a2072911d7a4e4a959b8f9614c7
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2026-04-30 15:38:43 +0300

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        rubik: ["Rubik", "sans-serif"],
+        open: ["Open Sans", "sans-serif"],
+      },
+
+      colors: {
+        secondary: "#F9BF29",
+        primary: "#35736E",
+        lightColor: "#D9D9D9",
+        grayColor: "#6A6A6A",
+      },
+    },
+  },
+  plugins: [],
+};
